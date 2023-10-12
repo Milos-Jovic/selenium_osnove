a6b6f28b700615a825ce7d57b9ed1b7be688e775
Milos-Jovic <milosjovic392@gmail.com>
2023-10-12 20:32:50 +0200

    SwagLabsTests - "TabelPage"

--- src/main/java/pages/TabelPage.java
+++ src/main/java/pages/TabelPage.java
@@ -1,2 +1,44 @@
-package pages;public class tabelPage {
+package pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class TabelPage extends BasicPage{
+
+    public TabelPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public int getRowNumber() {
+            return driver.findElements(By.cssSelector("tbody tr")).size();
+        }
+
+    public int getVisibleRowNumber() {
+        return driver.findElements(By.cssSelector("tbody tr")).size();
+    }
+
+
+    public void clickOnDeleteButtonByRowIndex(int rowIndex){
+        driver.findElements(By.className("delete")).get(rowIndex).click();
+    }
+
+        public String getFirstNameColumnValue(int rowIndex){
+        return driver.findElements(By.xpath("//tbody/tr/td[2]"))
+                .get(rowIndex).getText();
+        }
+    public String getLastNameColumnValue(int rowIndex){
+        return driver.findElements(By.xpath("//tbody/tr/td[3]"))
+                .get(rowIndex).getText();
+    }
+
+    public String getMiddleNameColumnValue(int rowIndex){
+        return driver.findElements(By.xpath("//tbody/tr/td[4]"))
+                .get(rowIndex).getText();
+    }
+
 }
