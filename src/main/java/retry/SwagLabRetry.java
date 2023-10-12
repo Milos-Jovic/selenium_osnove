713b5037fcfe21cd80471f4ee256f5e487b56597
Milos-Jovic <milosjovic392@gmail.com>
2023-10-12 20:32:34 +0200

    SwagLabsTests - "Retry"

--- src/main/java/retry/SwagLabRetry.java
+++ src/main/java/retry/SwagLabRetry.java
@@ -1,2 +1,18 @@
-package retry;public class SwagLabRetry {
+package retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class SwagLabRetry implements IRetryAnalyzer {
+    private int count = 0;
+    private int maxCount = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxCount) {
+            count++;
+            return true;
+        }
+        return false;
+    }
 }
