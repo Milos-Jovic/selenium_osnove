aa6164e8a941d9fe831fe333a9a8dba5bf937ef2
Milos-Jovic <milosjovic392@gmail.com>
2023-10-12 20:30:47 +0200

    SwagLabsTests - "ChackoutPage"

---
+++ src/main/java/pages/CheckoutPage.java
@@ -0,0 +1,16 @@
+package pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CheckoutPage extends BasicPage{
+    public CheckoutPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public void waitUntilUrlContainsPageName () {
+        wait
+                .withMessage("Url should contain page name.")
+                .until(ExpectedConditions.urlContains("checkout"));
+    }
+}
